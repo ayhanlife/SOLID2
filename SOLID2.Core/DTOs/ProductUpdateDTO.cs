017458fdfe7df4e84e4d01e4808a928df9f07ded
Ayhan YILDIZ <ayhanyldiz@hotmail.com>
2023-06-01 01:20:43 +0300

    eneme 1

--- SOLID2.Core/DTOs/ProductUpdateDTO.cs
+++ SOLID2.Core/DTOs/ProductUpdateDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Formats.Tar;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace SOLID2.Core.DTOs
         //v5
         //v5
         //v5
+        //V7TarEntryv8
         public int CategoryId { get; set; }
     }
 }
